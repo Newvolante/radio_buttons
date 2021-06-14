b02d131557a6ac2c421342703281365da7d5b501
Newvolante <60291015+Newvolante@users.noreply.github.com>
2021-06-14 12:42:03 +0200

    configuring variables - hiding the other textbox as the page is loaded

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 (function() {
     console.warn('Script linked correctly to the web page');
+
+    // nodelist with the first 2 options
+    let options = document.forms[0].elements.option;
+
+    // other checkbox
+    let otherCheckbox = document.forms[0].elements.other_checkbox;
+
+    // other text
+    let otherTextbox = document.getElementById('other-text');
+
+    // hides the other textbox as the page loads
+    otherTextbox.className = "hidden";
 })();
